89bd7bc30240d2ee3af983bb33a0b19e19e32054
William Godard <grenier.godard@gmail.com>
2019-04-08 16:04:21 +0200

    Add emojione

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import {emojify} from 'react-emojione';
 
 const Message = (props) => {
   const { author, content, created_at } = props.message;
   const date = new Date(created_at).toLocaleTimeString();
+  const options = {
+    style: {
+        height: 22,
+        margin: 4,
+    },
+  };
 
   return (
     <div className="message">
+      <div>
+        <span><strong>{author}</strong> à {date}</span>
+      </div>
       <p>
-        <span><strong>{author}</strong> à {date}</span><br />
-        {content}
+        {emojify(content, options)}
       </p>
     </div>
   );
